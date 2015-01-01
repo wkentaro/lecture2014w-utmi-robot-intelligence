cb651191ba3b68728f4e56c0942d695bdbf9c0fb
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-02 03:04:12 +0900

    add noise_test var and mean on the graph figure

--- code/noise_test_mnist.py
+++ code/noise_test_mnist.py
@@ -17,20 +17,27 @@ def noise_test():
     x = []
     n_samples = 70000
     for cl in np.arange(0, 26) * 0.01:
-        score = test_mnist(corruption_level=cl, epochs=n_samples, verbose=False)
+        score, _ = test_mnist(corruption_level=cl, epochs=n_samples, verbose=False)
         scores.append(score)
         x.append(cl)
     scores = np.array(scores)
     x = np.array(x)
 
-    # save scores
-    np.savez('../output/noise_test_mnist_scores_{0}.npz'.format(n_samples), scores)
+    ax1 = plt.subplot()
+    ax1.plot(x, scores)
+    ax1.set_title('noise and score with {0}'.format(n_samples))
+    ax1.set_xlabel('noise level')
+    ax1.set_ylabel('score')
 
-    ax = plt.subplot()
-    ax.plot(x, scores)
-    ax.set_title('noise and score with {0}'.format(n_samples))
-    ax.set_xlabel('noise')
-    ax.set_ylabel('score')
+    ax2 = plt.subplot()
+    label_text = r"""
+        $\mu = %.5f$
+        $\sigma = %.5f$
+        """ % (scores.mean(), scores.std())
+    label = ax2.text(0.05, 0.05, label_text,
+            horizontalalignment='left',
+            verticalalignment='bottom',
+            transform=ax2.transAxes)
     plt.savefig('../output/noise_test_mnist_{0}.png'.format(n_samples))
 
 
