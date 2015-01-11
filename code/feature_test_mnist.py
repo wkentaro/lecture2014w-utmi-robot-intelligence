78bfa47858446d86fde17c6ae12ea4aeb4da7416
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-11 18:41:14 +0900

    add feature_test_mnist (which have good score)

--- code/feature_test_mnist.py
+++ code/feature_test_mnist.py
@@ -3,6 +3,9 @@
 # feature_test_mnist.py
 # author: Kentaro Wada <www.kentaro.wada@gmail.com>
 
+from __future__ import print_function
+import sys
+
 import numpy as np
 import cv2
 
@@ -17,58 +20,49 @@ from sklearn.metrics import (
 
 import nn
 
-from dA import dA
+from autoencoder import AutoEncoder
 
-np.seterr(all='ignore')
-
-
-def get_dA_hidden(X,
-                  corruption_level,
-                  learning_rate=0.1,
-                  training_epochs=50):
-    # # load train data
-    # mnist = fetch_mldata('MNIST original')
-    # X = mnist.data
-    # construct dA
-    ni = X.shape[1]
-    # nh = int(0.06*ni)
-    nh = 100
-    da = dA(input=X, n_visible=ni, n_hidden=nh)
-    # train dA
-    for epoch in xrange(training_epochs):
-        da.train(lr=learning_rate, corruption_level=corruption_level)
-        # cost = da.negative_log_likelihood(corruption_level=corruption_level)
-        # print >> sys.stderr, 'Training epoch %d, cost is ' % epoch, cost
-        # learning_rate *= 0.95
-    # get hidden layer
-    X_hidden = da.get_hidden_values(input=X)
-    return X_hidden
 
 
 def feature_test_mnist(verbose=True):
+    print("... loading date")
     # load train data
     mnist = fetch_mldata('MNIST original')
     X_origin = mnist.data
     y = mnist.target
     target_names = np.unique(y)
+    print("--- done")
 
+    print("... random sampling & standardizing")
     p = np.random.randint(0, len(X_origin), 10000)
-    X_origin = X_origin[p][:10000]
-    y = y[p][:10000]
+    X_origin = X_origin[p]
+    y = y[p]
     # standardize
     X_origin = X_origin.astype(np.float64)
     X_origin /= X_origin.max()
     # print X_origin.min(), X_origin.mean(), X_origin.max(), X_origin.shape
+    print("--- done")
 
+    print("... encoding with denoising auto-encoder")
     # get feature & create input
-    X = get_dA_hidden(X=X_origin, corruption_level=0.1)
+    import theano.tensor as T
+    ae = AutoEncoder(X=X_origin,
+                     hidden_size=500,
+                     activation_function=T.nnet.sigmoid,
+                     output_function=T.nnet.sigmoid)
+    ae.train(n_epochs=20, mini_batch_size=20)
+    X = ae.get_hidden(data=X_origin)[0]
+    print("--- done")
 
     # get classifier
-    nh = int(0.16 * X.shape[1])
     clf = nn.NN(ni=X.shape[1],
-                nh=nh,
+                nh=int(0.1*X.shape[1]),
                 no=len(target_names),
-                corruption_level=0.04)
+                learning_rate=0.3,
+                inertia_rate=0.0,
+                corruption_level=0.0,
+                noise_level=0.0,
+                epochs=10000)
 
     # split data to train & test
     X_train, X_test, y_train, y_test = train_test_split(X, y)
@@ -79,21 +73,15 @@ def feature_test_mnist(verbose=True):
 
     clf.fit(X=X_train,
             y_train=label_train,
-            learning_rate=0.3,
-            inertia_rate=0.24,
-            epochs=70000)
+            )
 
-    y_pred = np.zeros(len(X_test))
-    for i, xt in enumerate(X_test):
-        o = clf.predict(xt)
-        y_pred[i] = np.argmax(o)
-    # print y_pred
+    y_pred = clf.predict(X_test)
 
     score = accuracy_score(y_true=y_test, y_pred=y_pred)
     if verbose is True:
-        print classification_report(y_true=y_test, y_pred=y_pred)
-        print confusion_matrix(y_true=y_test, y_pred=y_pred)
-        print score
+        print(classification_report(y_true=y_test, y_pred=y_pred))
+        print(confusion_matrix(y_true=y_test, y_pred=y_pred))
+        print(score)
 
     return score, clf
 
