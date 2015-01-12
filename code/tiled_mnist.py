fe8b99350f52391d7cceb019682cbccaf8d70093
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-12 12:54:05 +0900

    Add noise to the tiled plotting

--- code/tiled_mnist.py
+++ code/tiled_mnist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-# plot_mnist.py
+# tiled_mnist.py
 # author: Kentaro Wada <www.kentaro.wada@gmail.com>
 
 import numpy as np
@@ -10,20 +10,29 @@ from sklearn.datasets import fetch_mldata
 from utils import tile_raster_images
 
 
-def plot_mnist():
+def tiled_mnist(noise_level=0.0):
     # load data
     mnist = fetch_mldata('MNIST original')
+    X = mnist.data
+    X = X.astype(np.float64)
+    X /= X.max()
+
+    # add noise to the image
+    p = np.random.binomial(n=1, p=1-noise_level, size=X.shape)
+    X[p==0] = np.random.random(X.shape)[p==0]
 
     # get tiled image
-    p = np.random.randint(0, len(mnist.data), 400)
-    tile = tile_raster_images(mnist.data[p], (28,28), (20,20), scale_rows_to_unit_interval=False, output_pixel_vals=False)
+    p = np.random.randint(0, len(X), 400)
+    # tile = tile_raster_images(X[p], (28,28), (20,20), scale_rows_to_unit_interval=True, output_pixel_vals=True)
+    tile = tile_raster_images(X[p], (28,28), (20,20), scale_rows_to_unit_interval=True, output_pixel_vals=False)
 
     # save tiled data's image
     plt.axis('off')
     plt.title('MNIST dataset')
     plt.imshow(tile, cmap=plt.cm.gray_r)
-    plt.savefig('../output/tiled_mnist.png')
+    plt.savefig('../output/tiled_mnist_nl{0}.png'.format(noise_level))
 
 
 if __name__ == '__main__':
-    plot_mnist()
+    for nl in [0.0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+        tiled_mnist(noise_level=nl)
