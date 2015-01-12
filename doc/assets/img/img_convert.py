b8649b49e554ad2e38c9f9aa658a77a244246ca5
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-13 04:11:57 +0900

    update latex assets/imgs

--- doc/assets/img/img_convert.py
+++ doc/assets/img/img_convert.py
@@ -10,5 +10,5 @@ for file in files:
     base, ext = os.path.splitext(file)
     if ext in ['.png', '.jpeg', '.jpg']:
         os.system('convert {0} {1}.eps'.format(file, base))
-        os.system('convert {0} {1}.pdf'.format(file, base))
-        os.system('extractbb {0}'.format(file))
+        # os.system('convert {0} {1}.pdf'.format(file, base))
+        # os.system('extractbb {0}'.format(file))
