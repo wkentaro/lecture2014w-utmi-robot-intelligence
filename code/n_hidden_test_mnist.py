95c97fbfed93ebfe58921e94addd4172e367e494
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-12 12:16:01 +0900

    Complete n_hidden_test_mnist with new learning_rate, inertia_rate, epochs. And display the weights with tile_raster_images function.

--- code/n_hidden_test_mnist.py
+++ code/n_hidden_test_mnist.py
@@ -6,29 +6,32 @@
 from __future__ import print_function
 import time
 import cPickle
+import gzip
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from test_mnist import test_mnist
+from utils import tile_raster_images
 
 
 def n_hidden_test_mnist():
     print("... doing n_hidden test")
     # least hidden layer n_neuron analysis
     scores, x, nns = [], [], []
-    n_samples = 70000
-    for nh in np.arange(1, 40) * 0.02:
+    epochs = 150000
+    for nh in np.arange(1, 20) * 0.04:
         print("...... n_hidden: {0}".format(nh), end='')
         score, nn = test_mnist(nh=nh,
-                               corruption_level=0.04,
+                               corruption_level=0.0,
+                               noise_level=0.0,
                                learning_rate=0.3,
-                               inertia_rate=0.24,
-                               epochs=n_samples)
+                               inertia_rate=0.12,
+                               epochs=epochs)
         scores.append(score)
         x.append(nh)
         nns.append(nn)
-        print("score: {0}".format(score))
+        print(" score: {0}".format(score))
     scores, x = np.array(scores), np.array(x)
     print("--- done")
 
@@ -37,22 +40,30 @@ def n_hidden_test_mnist():
     # plot
     ax = plt.subplot()
     ax.plot(x, scores)
-    ax.set_title('n_hidden and score with {0}'.format(n_samples))
+    ax.set_title('n_hidden and score with {0}'.format(epochs))
     ax.set_xlabel('number of hidden layer neurons')
     ax.set_ylabel('score')
-    plt.savefig('../output/n_hidden_test_mnist_score_{0}.png'.format(n_samples))
+    plt.savefig('../output/n_hidden_test_mnist_score_{0}.png'.format(epochs))
     print("...... showing hidden layer weights as images")
     # about minimal hidden layer neurons
     for index in [np.argmax(scores), np.argmin(scores)]:
         W = nns[index].wi
+        W = np.delete(W, 0, axis=1)
         n_hidden = x[index]
-        p = np.arange(0, 25)
-        for i, w in enumerate(W[p]):
-            ax = plt.subplot(5, 5, i+1)
-            ax.axis('off')
-            w = np.delete(w, 0)
-            ax.imshow(w.reshape((28,28)), cmap=plt.cm.gray_r, interpolation='nearest')
-        plt.savefig('../output/n_hidden_test_mnist_image_nsmpl{0}_nh{1}.png'.format(n_samples, n_hidden))
+        p = np.random.randint(0, len(W), 400)
+        tile = tile_raster_images(W[p], (28,28), (20,20), scale_rows_to_unit_interval=True, output_pixel_vals=True, tile_spacing=(1,1))
+        # save tiled data's image
+        plt.axis('off')
+        plt.imshow(tile, cmap=plt.cm.gray_r)
+        plt.savefig('../output/n_hidden_test_mnist_image_nsmpl{0}_nh{1}.png'.format(epochs, n_hidden))
+    print("--- done")
+
+    print("... saving the results")
+    dump_data = {'n_hidden': x,
+                 'score': scores,
+                 'nn': nns}
+    with gzip.open('../output/n_hidden_test_mnist.pkl.gz', 'wb') as f:
+        cPickle.dump(dump_data, f)
     print("--- done")
 
 
